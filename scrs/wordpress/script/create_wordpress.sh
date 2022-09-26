7686e7ffd28afb29c6967c25775c9598109d92ac
vincentbachelet-collab <vcbachelet@gmail.com>
2022-09-26 18:03:45 +0200

    nginx && mariadb work

--- scrs/wordpress/script/create_wordpress.sh
+++ scrs/wordpress/script/create_wordpress.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 
+#check if wp-config.php exist
 if [ -f ./wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
-	#Download wordpress
+	#Download wordpress and all config file
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	#Update configuration file and move it to the php directory
-	#rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-
-	#Import env variables in the config file
-	#cd /var/www/html/wordpress
+	#Inport env variables in the config file
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE:/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
 fi
 
 exec "$@"
