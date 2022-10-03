5863229dcfeac053e7eec0b9beffc9939852290e
vincentbachelet-collab <vcbachelet@gmail.com>
2022-10-03 15:20:32 +0200

    redis ongoing

--- scrs/requirements/wordpress/tools/create_wordpress.sh
+++ scrs/requirements/wordpress/tools/create_wordpress.sh
@@ -27,11 +27,11 @@ else
 
 ## redis ##
 
-	wp config set WP_REDIS_HOST redis
-  	wp config set WP_REDIS_PORT 6379 --raw
- 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME
-  	wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD
- 	wp config set WP_REDIS_CLIENT phpredis
+	wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
+  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+ 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+  	wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+ 	wp config set WP_REDIS_CLIENT phpredis --allow-root
 ###  end of redis part  ###
 
 ###################################
