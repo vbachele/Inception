8865c0687ba7e90b5e06cd2a81bd4cb5b22ca773
vincentbachelet-collab <vcbachelet@gmail.com>
2022-10-03 12:21:54 +0200

    redis ongoing

--- scrs/requirements/bonus/redis/tools/redis.sh
+++ scrs/requirements/bonus/redis/tools/redis.sh
@@ -1,5 +1,14 @@
+#!/bin/sh
 
-if ("Authentication Unique Keys and Salts")
-echo ""
-define('WP_CACHE', true);
-define('WP_CACHE_KEY_SALT', 'yourwebsite.com');
+if [ ! -f "/etc/redis.conf.bak" ]; then
+
+    cp /etc/redis.conf /etc/redis.conf.bak
+
+    sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g" /etc/redis.conf
+    # sed -i "s|# requirepass foobared|requirepass $REDIS_PWD|g" /etc/redis.conf
+    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis.conf
+    sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis.conf
+
+fi
+
+redis-server --protected-mode no
