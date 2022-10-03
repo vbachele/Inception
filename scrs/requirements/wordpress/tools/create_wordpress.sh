cadc02c71a740812ce0a441b35d7d5eb1b355bf2
vincentbachelet-collab <vcbachelet@gmail.com>
2022-10-03 15:02:27 +0200

    redis ongoing

--- scrs/requirements/wordpress/tools/create_wordpress.sh
+++ scrs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
 #check if wp-config.php exist
-#if [ -f ./wp-config.php ]
-#then
-	#echo "wordpress already downloaded"
-#else
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
 
 ####### MANDATORY PART ##########
 
 	#Download wordpress and all config file
-	# wget http://wordpress.org/latest.tar.gz
-	# tar xfz latest.tar.gz
-	# mv wordpress/* .
-	# rm -rf latest.tar.gz
-	# rm -rf wordpress
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
 
-	# #Inport env variables in the config file
-	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	# cp wp-config-sample.php wp-config.php
+	#Inport env variables in the config file
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
 ###################################
 
 ####### BONUS PART ################
 
 ## redis ##
+
 	wp config set WP_REDIS_HOST redis
   	wp config set WP_REDIS_PORT 6379 --raw
  	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME
@@ -34,6 +35,6 @@
 ###  end of redis part  ###
 
 ###################################
-#fi
+fi
 
 exec "$@"
