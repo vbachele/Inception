8bdfba69c57f5f29bf167b6527dc3603fc63c979
vincentbachelet-collab <vcbachelet@gmail.com>
2022-10-03 12:47:51 +0200

    redis ongoing

--- scrs/requirements/bonus/redis/tools/redis.sh
+++ scrs/requirements/bonus/redis/tools/redis.sh
@@ -4,10 +4,10 @@ if [ ! -f "/etc/redis/redis.conf.bak" ]; then
 
     cp /etc//redis/redis.conf /etc/redis/redis.conf.bak
 
-    sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g" /etc/redis.conf
+    sed -i "s|bind 127.0.0.1|#bind 127.0.0.1|g" /etc/redis/redis.conf
     # sed -i "s|# requirepass foobared|requirepass $REDIS_PWD|g" /etc/redis.conf
-    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis.conf
-    sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis.conf
+    sed -i "s|# maxmemory <bytes>|maxmemory 2mb|g" /etc/redis/redis.conf
+    sed -i "s|# maxmemory-policy noeviction|maxmemory-policy allkeys-lru|g" /etc/redis/redis.conf
 
 fi
 
