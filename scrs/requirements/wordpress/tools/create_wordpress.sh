3667b47576ae4530bf61e327dd475252785df653
vincentbachelet-collab <vcbachelet@gmail.com>
2022-10-03 15:00:23 +0200

    redis ongoing

--- scrs/requirements/wordpress/tools/create_wordpress.sh
+++ scrs/requirements/wordpress/tools/create_wordpress.sh
@@ -9,18 +9,18 @@
 ####### MANDATORY PART ##########
 
 	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+	# wget http://wordpress.org/latest.tar.gz
+	# tar xfz latest.tar.gz
+	# mv wordpress/* .
+	# rm -rf latest.tar.gz
+	# rm -rf wordpress
 
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+	# #Inport env variables in the config file
+	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	# cp wp-config-sample.php wp-config.php
 ###################################
 
 ####### BONUS PART ################
