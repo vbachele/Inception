cfd02b2ebe3862c9f66e13ba5a232031c0a89602
vincentbachelet-collab <vcbachelet@gmail.com>
2022-10-04 15:22:04 +0200

    ongoing ftp_server

--- scrs/requirements/bonus/ftp_server/tools/server_ftp.sh
+++ scrs/requirements/bonus/ftp_server/tools/server_ftp.sh
@@ -9,10 +9,10 @@ if [ ! -f "/etc/vsftpd.conf.bak" ]; then
 	mv /tmp/vsftpd.conf /etc/vsftpd.conf
 
 	#Add the FTP_USER, change his password and declare him as the owner of wordpress folder and all subfolders
-    adduser $FTP_USR --disabled-password
-    echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
-	chown -R $FTP_USR:$FTP_USR /var/www/html
-    echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+    adduser $FTP_USER --disabled-password
+    echo "$FTP_USER:$FTP_PASSWORD" | /usr/sbin/chpasswd &> /dev/null
+	chown -R $FTP_USER:$FTP_USER /var/www/html
+    echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
 
 
 fi
