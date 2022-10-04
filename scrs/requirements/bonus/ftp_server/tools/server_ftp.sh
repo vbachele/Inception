f19fee00fcea42378155f01e0053123ea229f623
vincentbachelet-collab <vcbachelet@gmail.com>
2022-10-04 14:52:11 +0200

    ongoing ftp_server

--- scrs/requirements/bonus/ftp_server/tools/server_ftp.sh
+++ scrs/requirements/bonus/ftp_server/tools/server_ftp.sh
@@ -11,9 +11,8 @@ if (! -f "/etc/vsftpd.conf.bak"); then
     # echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
 	# chown -R $FTP_USR:$FTP_USR /var/www/html
     # echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+
 fi
 
 ##echo "FTP started on :21"
 #/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
-
-fi
